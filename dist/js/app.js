f25cc65fe29c616f9957500ce92b245003e8a105
kumari-madhuri <59887837+kumari-madhuri@users.noreply.github.com>
2021-09-04 11:13:43 +0200

    animation added

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 gsap.from(".nav-links", { duration: 1, opacity: 0 });
 gsap.from(".hero-img", { duration: 1, opacity: 0, scale: 0.5, delay: 1 });
 gsap.from(".scroll-down", {
@@ -6,4 +7,37 @@ gsap.from(".scroll-down", {
   ease: "bounce.out",
   delay: 2,
   repeat: 1,
+});
+gsap.from(".center", { duration: 1, opacity: 0, scale: 0.5, delay: 1 });
+
+gsap.from(".right", {
+  scrollTrigger: {
+    trigger: ".right",
+    start: "top 80%",
+    toggleActions: "restart none none reset",
+  },
+  x: 150,
+  opacity: 0,
+  duration: 1.5,
+});
+gsap.from(".left", {
+  scrollTrigger: {
+    trigger: ".left",
+    start: "top 80%",
+    toggleActions: "restart none none reset",
+  },
+  x: -150,
+  opacity: 0,
+  duration: 1.5,
+});
+gsap.from(".bottom", {
+  scrollTrigger: {
+    trigger: ".bottom",
+    start: "top 90%",
+    // markers: true,
+    toggleActions: "restart none none reset",
+  },
+  y: 150,
+  opacity: 0,
+  duration: 1.5,
 });
